e8d7a1c6c369c45e2eeb9649947966d5da971e3b
Zhi LIU <liuzhi0210@gmail.com>
2017-11-02 13:24:08 +0100

    SAP and Outcast.

--- wordnet/Outcast.java
+++ wordnet/Outcast.java
@@ -2,9 +2,12 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Outcast {
+
+    private WordNet wordNet;
+
     public Outcast(WordNet wordnet)         // constructor takes a WordNet object
     {
-
+        this.wordNet = wordnet;
     }
 
     public static void main(String[] args) {
@@ -17,8 +20,28 @@ public class Outcast {
         }
     }
 
+    private int sumDist(String noun, String[] nouns) {
+        int dist = 0;
+        for (String n : nouns) {
+            if (!n.equals(noun)) {
+                dist += wordNet.distance(n, noun);
+            }
+        }
+        return dist;
+    }
+
     public String outcast(String[] nouns)   // given an array of WordNet nouns, return an outcast
     {
-        return null;
+        int maxDist = Integer.MIN_VALUE;
+        String out = null;
+        for (String n :
+                nouns) {
+            int dist = sumDist(n, nouns);
+            if (dist > maxDist) {
+                out = n;
+                maxDist = dist;
+            }
+        }
+        return out;
     }
 }
--- wordnet/SAP.java
+++ wordnet/SAP.java
@@ -1,13 +1,18 @@
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.Collections;
+
 public class SAP {
+
+    private Digraph digraph;
 
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph G) {
-
+        this.digraph = new Digraph(G);
     }
 
     // do unit testing of this class
@@ -29,18 +34,44 @@ public class SAP {
         return -1;
     }
 
+    private int[] sap(Iterable<Integer> v, Iterable<Integer> w) {
+        BreadthFirstDirectedPaths pathsV = new BreadthFirstDirectedPaths(this.digraph, v);
+        BreadthFirstDirectedPaths pathsW = new BreadthFirstDirectedPaths(this.digraph, w);
+        int numNode = this.digraph.V();
+        int[] info = new int[2];
+        info[0] = -1;
+        info[1] = Integer.MAX_VALUE;
+        for (int n = 0; n < numNode; n++) {
+            if (pathsV.hasPathTo(n) && pathsW.hasPathTo(n)) {
+                int distN = pathsV.distTo(n) + pathsW.distTo(n);
+                if (distN < info[1]) {
+                    info[0] = n;
+                    info[1] = distN;
+                }
+            }
+        }
+        return info;
+    }
+
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
     public int ancestor(int v, int w) {
-        return -1;
+        int[] sap = sap(Collections.singletonList(v), Collections.singletonList(w));
+        return sap[0];
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
-        return -1;
+        int[] sap = sap(v, w);
+        if (sap[0] == -1) {
+            return -1;
+        } else {
+            return sap[1];
+        }
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-        return -1;
+        int[] sap = sap(v, w);
+        return sap[0];
     }
 }
