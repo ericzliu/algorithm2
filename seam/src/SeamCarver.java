32bccb6a36f499f2507e6c116c7e99fa802665fc
Zhi LIU <liuzhi0210@gmail.com>
2017-11-08 23:59:43 +0100

    Calculate vertical seam for one point.

--- seam/src/SeamCarver.java
+++ seam/src/SeamCarver.java
@@ -1,13 +1,25 @@
+import edu.princeton.cs.algs4.AcyclicSP;
+import edu.princeton.cs.algs4.DirectedEdge;
+import edu.princeton.cs.algs4.EdgeWeightedDigraph;
 import edu.princeton.cs.algs4.Picture;
 
-import java.awt.*;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class SeamCarver {
     private Picture origin;
     private Picture current;
 
+    private void assertNonNull(Object object) {
+        if (null == object) {
+            throw new IllegalArgumentException();
+        }
+    }
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
+        assertNonNull(picture);
         this.origin = picture;
         this.current = new Picture(picture);
     }
@@ -65,12 +77,107 @@ public class SeamCarver {
 
     // remove horizontal seam from current picture
     public void removeHorizontalSeam(int[] seam) {
-
+        if (height() <= 1) {
+            throw new IllegalArgumentException();
+        }
+        assertNonNull(seam);
+        assertValidSeam(seam);
+        int length = seam.length;
+        if (length != width()) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < length; i++) {
+            guardRow(seam[i]);
+        }
     }
 
     // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
+        if (width() <= 1) {
+            throw new IllegalArgumentException();
+        }
+        assertNonNull(seam);
+        assertValidSeam(seam);
+        int length = seam.length;
+        if (length != height()) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < length; i++) {
+            guardColumn(seam[i]);
+        }
+        for (int x = 0; x < width(); x++) {
 
+        }
+    }
+
+    private Seam verticalSeam(int x) {
+        int height = height();
+        int width = width();
+        int id = 0;
+        List<Pt> pts = new ArrayList<>();
+        List<DirectedEdge> edges = new ArrayList<>();
+        HashMap<Integer, Pt> lastLine = new HashMap<>();
+        HashMap<Integer, Pt> line = new HashMap<>();
+        for (int y = 0; y < height; y++) {
+            int lo = Math.max(x - y, 0);
+            int hi = Math.min(x + y, width - 1);
+            line = new HashMap<>();
+            for (int c = lo; c <= hi; c++) {
+                Pt pt = new Pt(id++, c, y);
+                pts.add(pt);
+                line.put(c, pt);
+                double energy = energy(c, y);
+                if (lastLine.containsKey(c - 1)) {
+                    edges.add(new DirectedEdge(lastLine.get(c - 1).id, pt.id, energy));
+                }
+                if (lastLine.containsKey(c)) {
+                    edges.add(new DirectedEdge(lastLine.get(c).id, pt.id, energy));
+                }
+                if (lastLine.containsKey(c + 1)) {
+                    edges.add(new DirectedEdge(lastLine.get(c + 1).id, pt.id, energy));
+                }
+            }
+            lastLine = line;
+        }
+        EdgeWeightedDigraph digraph= new EdgeWeightedDigraph(pts.size());
+        for (DirectedEdge edge: edges) {
+            digraph.addEdge(edge);
+        }
+        double dist = Double.MAX_VALUE;
+        int dest = -1;
+        AcyclicSP sp = new AcyclicSP(digraph, 0);
+        for (Pt pt : line.values()) {
+            int ptId = pt.getId();
+            if (sp.hasPathTo(ptId)) {
+                double distTo = sp.distTo(ptId);
+                if (distTo < dist) {
+                    dist = distTo;
+                    dest = ptId;
+                }
+            }
+        }
+        if (dest != -1) {
+            int[] seam = new int[height];
+            seam[0] = x;
+            Iterable<DirectedEdge> path = sp.pathTo(dest);
+            int loc = 1;
+            for (DirectedEdge e : path) {
+                int to = e.to();
+                Pt pt = pts.get(to);
+                seam[loc++] = pt.getId();
+            }
+            return new Seam(dist, seam);
+        }
+        throw new RuntimeException("No shortest path found.");
+    }
+
+    private void assertValidSeam(int[] seam) {
+        int length = seam.length;
+        for (int i = 1; i < length; i++) {
+            if (Math.abs(seam[i] - seam[i - 1]) > 1) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 
     private void guardColumn(int x) {
@@ -82,6 +189,48 @@ public class SeamCarver {
     private void guardRow(int y) {
         if (y < 0 || y >= this.height()) {
             throw new IllegalArgumentException("Row out of range.");
+        }
+    }
+
+    private static class Pt {
+        private int id;
+        private int x; // column
+        private int y; // row
+
+        public Pt(int id, int x, int y) {
+            this.id = id;
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+    }
+
+    private static class Seam {
+        private double dist;
+        private int[] seam;
+
+        public Seam(double dist, int[] seam) {
+            this.dist = dist;
+            this.seam = seam;
+        }
+
+        public double getDist() {
+            return dist;
+        }
+
+        public int[] getSeam() {
+            return seam;
         }
     }
 }
